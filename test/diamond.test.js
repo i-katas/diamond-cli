85ae3fdaa17574d2e1a3f50537ecaedc21cd98d2
selonj <selonj@163.com>
2019-07-29 21:18:22 +0800

    test driven by incremental development

--- test/diamond.test.js
+++ test/diamond.test.js
@@ -1,7 +1,46 @@
 import diamond from 'diamond';
 
 describe('diamond', () => {
-    it('single layered diamond', () => {
-        expect(diamond('A')).toBe('A');
+    describe('whole presentations', () => {
+        it('diamond A', () => {
+            expect(diamond('A')).toBe('A');
+        });
+
+        it('diamond B', () => {
+            expect(diamond('B')).toBe(
+                ' A \n' +
+                'B B\n' +
+                ' A ' 
+            );
+        });
+
+        it('diamond C', () => {
+            expect(diamond('C')).toBe(
+                '  A  \n' +
+                ' B B \n' +
+                'C   C\n' +
+                ' B B \n' +
+                '  A  ' 
+            );
+        });
+    });
+
+    describe('top-half presentations', () => {
+        function topHalfDiamond(c) {
+            let lines = diamond(c).split('\n');
+            return lines.slice(0, (lines.length + 1)/2).join('\n');
+        }
+
+        it('diamond A', () => {
+            expect(topHalfDiamond('A')).toBe('A');
+        });
+
+        it('diamond B', () => {
+            expect(topHalfDiamond('B')).toBe(' A \nB B');
+        });
+
+        it('diamond C', () => {
+            expect(topHalfDiamond('C')).toBe('  A  \n B B \nC   C');
+        });
     });
 });
